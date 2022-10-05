9ece905f441c2d377b836696eca24029fee4a2d9
Nobi <67573296+iBlueFox@users.noreply.github.com>
2022-10-05 22:03:50 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,8 +14,8 @@ global.APIKeys = {
 
 // setting 
 global.autoread = false // auto read message
-global.autobio = true 
-global.userRegister = false 
+global.autobio = false
+global.userRegister = true
 global.autoblok212 = false //ubah jadi true biar nmr 212 di blok sama bot
 
 //language
